b42d508f5431c2e5044536a0003edeab8338f221
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-30 11:03:28 -0700

    Initial Commit

---
+++ FruitMatket_SwiftUI/FruitMatket_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMatket_SwiftUIApp.swift
+//  FruitMatket_SwiftUI
+//
+//  Created by Mitya Kim on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitMatket_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
